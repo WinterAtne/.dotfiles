55d54c421bf0feaafcf4a30e9aa01671513e2336
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2024-06-29 11:42:33 -0600

    Fixed font issues by changing font to non-monospaced, and changed a few icons

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 18;       /* gaps between windows */
+static const unsigned int gappx     = 16;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font Mono" };
-static const char dmenufont[]       = "Hack Nerd Font Mono";
+static const char *fonts[]          = { "Hack Nerd Font" };
+static const char dmenufont[]       = "Hack Nerd Font";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";  
@@ -44,9 +44,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]",      tile },    /* first entry is default */
-	{ "[]",      NULL },    /* no layout function means floating behavior */
-	{ "[󰊓]",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "󰊓",      monocle },
 };
 
 /* key definitions */
